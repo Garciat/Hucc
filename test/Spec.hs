afcdbc06a9c84715a14ca9c255d52fe9e4fa5b6d
Gabriel Garcia <garciat@live.com>
2015-08-23 15:59:30 -0400

    lex tests

---
+++ test/Spec.hs
@@ -0,0 +1,31 @@
+import Test.Hspec
+import Data.Either (isLeft)
+import Control.Applicative
+import Control.Exception (evaluate)
+import qualified Language.Python.Parser.Lexer as P
+
+main :: IO ()
+main = hspec $ do
+  describe "Language.Python.Parser.Lexer.lex" $ do
+    it "parses Python numeric literals" $ do
+      "0"     `lexesTo` [P.Number (P.IntLiteral 0)]
+      "0."    `lexesTo` [P.Number (P.FloatLiteral 0.0)]
+      ".0"    `lexesTo` [P.Number (P.FloatLiteral 0.0)]
+      "0.0"   `lexesTo` [P.Number (P.FloatLiteral 0.0)]
+      
+      "1"     `lexesTo` [P.Number (P.IntLiteral 1)]
+      "1."    `lexesTo` [P.Number (P.FloatLiteral 1.0)]
+      "1.0"   `lexesTo` [P.Number (P.FloatLiteral 1.0)]
+      "1.e0"  `lexesTo` [P.Number (P.FloatLiteral 1.0)]
+      
+    
+    it "does not parse invalid Python numeric literals" $ do
+      lexFailsFor "01"
+  
+  where
+    lexFailsFor input =
+      P.lex "<spec>" input `shouldSatisfy` isLeft
+    
+    lexesTo input tokens =
+      let result = map P.ptToken <$> P.lex "<spec>" input in
+      result `shouldBe` Right (tokens ++ [P.NewLine, P.EndMarker])
