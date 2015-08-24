536216081a434ac57e04a866922837a32b80ee88
Gabriel Garcia <garciat@live.com>
2015-08-24 14:37:45 -0400

    include keyword list

--- src/Language/Python/Parser/Lexer.hs
+++ src/Language/Python/Parser/Lexer.hs
@@ -280,6 +280,8 @@ parseToken = P.choice
   -- Identifiers
   -----------------------------------------------------------
   
+  -- TODO https://docs.python.org/3.5/reference/lexical_analysis.html#identifiers
+  
   parseName :: LexemeParser String
   parseName = (:) <$> identStart <*> P.many identLetter
   
@@ -442,4 +444,44 @@ parseToken = P.choice
                       parseEsc (c, code)    = do{ P.char c; return [code] }
   
   escMap = zip ("abfnrtv\\\"\'") ("\a\b\f\n\r\t\v\\\"\'")
-  
+
+-- |
+-- A list of Python keywords.
+--
+reservedNames :: [String]
+reservedNames = [ "False"
+                , "None"
+                , "True"
+                , "and"
+                , "as"
+                , "assert"
+                , "break"
+                , "class"
+                , "continue"
+                , "def"
+                , "del"
+                , "elif"
+                , "else"
+                , "except"
+                , "finally"
+                , "for"
+                , "from"
+                , "global"
+                , "if"
+                , "import"
+                , "in"
+                , "is"
+                , "lambda"
+                , "nonlocal"
+                , "not"
+                , "or"
+                , "pass"
+                , "raise"
+                , "return"
+                , "try"
+                , "while"
+                , "with"
+                , "yield"
+                ]
+
+--
